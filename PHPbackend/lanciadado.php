dc4ca1a326320f6ba923ed62c42167bffb47d069
Lorped <lorped@gmail.com>
2018-09-04 21:44:42 +0200

    fix usofdv/random

--- PHPbackend/lanciadado.php
+++ PHPbackend/lanciadado.php
@@ -1,4 +1,4 @@
-<? 
+<?
 
   //http://stackoverflow.com/questions/18382740/cors-not-working-php
   if (isset($_SERVER['HTTP_ORIGIN'])) {
@@ -6,20 +6,20 @@
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
   }
- 
+
   // Access-Control headers are received during OPTIONS requests
   if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
- 
+
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
- 
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
       header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
- 
+
     exit(0);
   }
 
-	
+
  header('Content-Type: text/html; charset=utf-8');
 
 
@@ -31,13 +31,13 @@ include ("db.inc.php");
   $idutente = $request->userid;
 
 
-		
+
 		$Mysql="SELECT nomepg FROM personaggio WHERE idutente=$idutente";
-		if ( $res=mysql_fetch_array(mysql_query($Mysql)) ) {	
-		$nomepg=$res['nomepg'];
+		if ( $res=mysql_fetch_array(mysql_query($Mysql)) ) {
+		  $nomepg= mysql_real_escape_string( $res['nomepg'] );
 		} else {
 			$nomepg="NARRAZIONE";
-		}	
+		}
 
 
 
