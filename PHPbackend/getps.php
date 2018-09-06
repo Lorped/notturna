9782bc600edc1d629c72684469e32cc90817effd
Lorped <lorped@gmail.com>
2018-09-07 01:11:57 +0200

    getps

--- PHPbackend/getps.php
+++ PHPbackend/getps.php
@@ -29,7 +29,10 @@
 
  	$idutente=$_GET['id'];
 
- 	$Mysql="SELECT PScorrenti, sete-PScorrenti as 'psvuoti' FROM personaggio LEFT JOIN statuscama ON personaggio.idstatus = statuscama.idstatus WHERE idutente=$idutente";
+ 	$Mysql="SELECT PScorrenti, sete+addsete-PScorrenti as 'psvuoti' FROM personaggio 
+		LEFT JOIN statuscama ON personaggio.idstatus = statuscama.idstatus
+		LEFT JOIN blood ON personaggio.bloodp = blood.bloodp
+		WHERE idutente=$idutente";
 	$Result=mysql_query ($Mysql);
 	$res=mysql_fetch_array($Result,MYSQL_ASSOC);
 	$output = json_encode($res);
