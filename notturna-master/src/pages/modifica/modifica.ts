1b8476260d5b2bcdb631910ba82b6076a86e5c4d
Lorenzo <lorped@gmail.com>
2019-09-13 13:29:28 +0200

    cont modifica oggetti

--- notturna-master/src/pages/modifica/modifica.ts
+++ notturna-master/src/pages/modifica/modifica.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 
+import { Oggetto, FullOggetto, Con, Condizione } from '../global.ts';
+
 /**
  * Generated class for the ModificaPage page.
  *
@@ -14,15 +16,22 @@ import { Http } from '@angular/http';
   selector: 'page-modifica',
   templateUrl: 'modifica.html',
 })
+
+
+
 export class ModificaPage {
 
   barcode: string;
 
+  myoggetto = new FullOggetto;
+
+
   constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
     this.barcode = this.navParams.get("parentPage").barcode;
 
-    console.log(this.barcode);
+    // console.log(this.barcode);
 
+    /***
     var url = 'http://www.roma-by-night.it/ionicPHP/barcode3.php?barcode='+this.barcode;
 
 		this.http.get(url)
@@ -30,9 +39,29 @@ export class ModificaPage {
 		.subscribe( data => {
 
 			console.log(data);
+
+      this.myoggetto=data;
 		});
 
+    ***/
+    this.loadoggetto();
+
+
   }
+
+  loadoggetto() {
+    var url = 'http://www.roma-by-night.it/ionicPHP/barcode3.php?barcode='+this.barcode;
+
+		this.http.get(url)
+		.map(data => data.json())
+		.subscribe( data => {
+
+			console.log(data);
+
+      this.myoggetto=data;
+		});
+  }
+
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad ModificaPage');
