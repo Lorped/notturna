ba9f70185ab49a7aaa5c312b104f40926ad92eae
Lorenzo <lorped@gmail.com>
2019-09-19 14:56:11 +0200

    lista cond a/s/p/d

--- notturna-master/src/pages/editcond/editcond.ts
+++ notturna-master/src/pages/editcond/editcond.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Oggetto, FullOggetto, Con, Condizione } from '../global';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the EditcondPage page.
@@ -23,13 +24,39 @@ export class EditcondPage {
     'Carisma', 'Persuasione', 'Saggezza',
     'Percezione', 'Intelligenza', 'Prontezza'] ;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  newcondA: number;
+  newcondD: number;
+  newcondP: number;
+  newcondS: number;
+
+  condD: Array<any>;
+  condP: Array<any>;
+  condS: Array<any>;
+
+
+  constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams) {
     this.condizione = this.navParams.get("condizione");
+    this.caricacond ();
     console.log(this.condizione);
   }
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad EditcondPage');
+  }
+
+  caricacond () {
+    var url = 'http://www.roma-by-night.it/ionicPHP/listpoteridiscipline.php';
+
+		this.http.get(url)
+      .map(data => data.json())
+    	.subscribe(res =>  {
+        console.log(res);
+        this.condD = res[0].discipline;
+        this.condP = res[0].poteri;
+        this.condS = res[0].skill;
+        console.log (this.condP);
+        console.log (this.condD);
+      });
   }
 
 }
